152fd5009fd545725e9233659152b2126501541a
issaZananiri <https://github.com/issaZananiri>
2020-12-26 15:26:56 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router} from 'react-router-dom';
+import { Route,Link} from 'react-router-dom';
+import  About from './components/About' ;
+import Home from './components/Home' ;
+import Fentities from './components/Fentities';
+import Fentity from './components/Fentity';
 
 class App extends Component {
   constructor() {
@@ -17,19 +23,26 @@ class App extends Component {
       ]
     }
   }
+
   
   render() {
     const state = this.state
     return (
-      <div className="App">
-        <div id="home-background"></div>
-        <div id="main-links">
-          {/* Main Links */}
-        </div>
-        {/* Routes go here v */}
+      <Router>
+        <div className="App">
+          <div id="home-background"></div>
+          <div id="main-links">
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
 
-        {/* Routes go here ^ */}
-      </div>
+
+          </div>
+            <Route path="/" exact component={Home} />
+            <Route path="/about" exact render={() => <About items={Object.keys(state)} />} />
+            <Route path="/directory/:fentities" exact render={({ match }) => <Fentities match={match} state={state} />}/>
+            <Route path="/directory/:fentities/:name" exact render={({match}) => <Fentity match={match} state={state}/>} />
+        </div>
+      </Router>
     );
   }
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,27 +1,19 @@
 import React, { Component } from 'react';
 
 import '../styles/home.css'
-
+import { BrowserRouter as Router} from 'react-router-dom';
+import { Route,Link} from 'react-router-dom';
 class Home extends Component {
 
     render() {
         return (
-            <div>
-                <div id="u-input">
-                    <input type="text" placeholder="Ask and you shall receive" />
-                    <div id="button">Seek</div>
-                </div>
+            <div><div id="u-input"><input type="text" placeholder="Ask and you shall receive" /><div id="button">Seek</div></div>
+
 
                 <h1 id="home-title">Your Adventure</h1>
 
-                <div id="home-container">
-                    <div id="world"><span className="main-directory-text">World</span></div>
-                    <div id="wizards"><span className="main-directory-text">Wizards</span></div>
-                    <div id="bestiary"><span className="main-directory-text">Bestiary</span></div>
-                    <div id="potions"><span className="main-directory-text">Potions</span></div>
-                    <div id="deities"><span className="main-directory-text">Deities</span></div>
-                </div>
-            </div>
+
+                <div id="home-container"><div id="world"><span className="main-directory-text">World</span></div><Link to="/directory/wizards"><div id="wizards"><span className="main-directory-text">Wizards</span></div></Link><Link to="/directory/bestiary"><div id="bestiary"><span className="main-directory-text">Bestiary</span></div></Link><div id="potions"><span className="main-directory-text">Potions</span></div><div id="deities"><span className="main-directory-text">Deities</span></div></div></div>
         );
     }
 }
